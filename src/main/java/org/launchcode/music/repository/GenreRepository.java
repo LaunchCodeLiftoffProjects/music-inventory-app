40f576d4482702c2b32a2e1d542e7913795c8f12
Michael2832 <51465623+Michael2832@users.noreply.github.com>
2019-11-15 17:08:57 -0600

    Genre (#2)
    * Setup but getting Null again
    
    * Getting Errors
    
    * Removed Constructor Error Gone

---
+++ src/main/java/org/launchcode/music/repository/GenreRepository.java
@@ -0,0 +1,11 @@
+package org.launchcode.music.repository;
+
+import org.launchcode.music.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
