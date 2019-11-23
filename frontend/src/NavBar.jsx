cd0c49b0a373d438860371dfc98429c22bcee867
Sowmya Ananth <sowmyaanantha@gmail.com>
2019-11-23 15:40:33 -0600

    Added a logo -- subject to change

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
 import { AppBar, Toolbar } from '@material-ui/core'
+import { mergeClasses } from '@material-ui/styles'
+import logo from './logo.jpg'
 
 export const NavBar = ({ color }) => (
+    
     <AppBar color={color}>
+    
         <Toolbar>
+        <img src={logo} alt="logo" width="42" height="42"></img>
+
             <Link to="/page1">
                 <Button>Playlist</Button>
             </Link>
@@ -14,9 +20,8 @@ export const NavBar = ({ color }) => (
             </Link>
             <Link to="/page3">
                 <Button>Search</Button>
-            </Link>
+            </Link>        
             
-             
              <Link to="/page4">
                 <Button>Share</Button>
              </Link>
