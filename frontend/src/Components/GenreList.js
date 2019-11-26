be24f009fd58d10e0c4b0e25e950433e2e3d972c
Michael Behle <michaelbehle@yahoo.com>
2019-11-25 20:19:59 -0600

    axios trial 2

---
+++ frontend/src/Components/GenreList.js
@@ -0,0 +1,22 @@
+import React from react ;
+import Axios from "axios";
+
+export default class GenreList extends React.Component {
+    state = {
+        genre: []
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:8080/genre").then(res => {
+            console.log(res);
+            this.setState({ genre: res.data });
+        });
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.state.genre.map(genre => <li>{genre.name}</li>)}
+            </ul>
+        )
+    }
