5c879cb00b17e80ca6c88e223f062efe9464a326
Cassie Anderson <cassieandersonmae@gmail.com>
2019-11-30 19:31:03 -0600

    still need to add track lists

--- frontend/src/pages/page1.jsx
+++ frontend/src/pages/page1.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 
-export default class PageOne extends React.Component {
-  //Write HTML inside render function
+
+export default class Track extends React.Component {
   render() {
-    return <div>Page 1</div>;
-  }
+  return (
+    <div>
+    <h1 color="white"> Create Playlist </h1>
+    
+      <form>
+        <table>
+          <tbody>
+          <tr>
+            <td>
+              Name for new Playlist:<br/>
+                <input type="text" name="playlist"/><br/>
+            </td>    
+          </tr>  
+          </tbody>
+        </table>
+        <table> Tracks:</table>
+        <input type= "submit"/>
+      </form>
+      </div>
+  );
+}
 }
--- frontend/src/pages/page2.jsx
+++ frontend/src/pages/page2.jsx
@@ -4,5 +4,6 @@ export default class PageTwo extends React.Component {
   //Write HTML inside render function
   render() {
     return <div>Page 2</div>;
+
   }
 }
