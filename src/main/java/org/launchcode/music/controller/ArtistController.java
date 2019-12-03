d9d9451fc84cec03079b5059954c14a67fdd7322
Michael Behle <michaelbehle@yahoo.com>
2019-12-02 19:10:50 -0600

    artist connected

--- src/main/java/org/launchcode/music/controller/ArtistController.java
+++ src/main/java/org/launchcode/music/controller/ArtistController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/artist")
 public class ArtistController {
     private ArtistService artistService ;
