c7c5471d5bb04778903197bc018e35f134e8d1d4
sowmyaananth2019 <51089315+sowmyaananth2019@users.noreply.github.com>
2019-12-07 19:43:41 -0600

    adding button (#12)
    * adding button
    
    * merge master
    
    *  nnnn

--- frontend/src/pages/page1.jsx
+++ frontend/src/pages/page1.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 
-export default class Track extends React.Component {
+function PlayListRender(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+      </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const numbers = ["Country", "Jazz", "Keith Urban", "Rock"];
+
+export default class Playlist extends React.Component {
   //Write HTML inside render function
   render() {
-    return <div>Track</div>;
+   return(
+     <div>
+       <div>Playlist</div>
+       <PlayListRender numbers={numbers}></PlayListRender> 
+           </div>
+          
+   );
+   
   }
 }
