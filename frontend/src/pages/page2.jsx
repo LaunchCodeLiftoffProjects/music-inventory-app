95e94a868b11a5907aaf02db083491ab6c6fc713
AshantiBloodsaw <bloodsaw2117@gmail.com>
2019-12-08 19:23:40 -0600

    commit quote

--- frontend/src/pages/page2.jsx
+++ frontend/src/pages/page2.jsx
@@ -1,46 +1,12 @@
 import React from "react";
-
 export default class Track extends React.Component {
   //Write HTML inside render function
   render() {
-    <html>
-    <head>
-    <title>View Songs Page</title>
-    
-    <h1>View Songs Page</h1>
-    </head>
-    <body>
-    <style>
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333333;
-}
-
-li {
-  float: left;
-}
-
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 16px;
-  text-decoration: none;
-}
-
-li a:hover {
-  background-color: #111111;
-}
-</style>
-    
-    
-    
-    
-    </body>
-    </html>
-    return <div>Track</div>;
+    return (
+      <div>
+        <title>View Songs Page</title>
+        <h1>View Songs Page</h1>
+      </div>
+    );
   }
 }
