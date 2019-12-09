5ceeb888e9be9f868af798859396d8aaa043089e
Michael2832 <51465623+Michael2832@users.noreply.github.com>
2019-12-09 17:56:21 -0600

    adding button (#12) (#13)
    * adding button
    
    * merge master
    
    *  nnnn

---
+++ frontend/src/pages/homePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class homePage extends React.Component {
+  //Write HTML inside render function
+  render() {
+    return (
+      <div>
+        <div>Playlist</div>
+      </div>
+    );
+  }
+}
