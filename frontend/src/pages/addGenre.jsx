d0daaa55105a48fe43074d9d9f19075fcd48a35c
Sowmya Ananth <sowmyaanantha@gmail.com>
2019-12-13 16:27:55 -0600

    help

--- frontend/src/pages/addGenre.jsx
+++ frontend/src/pages/addGenre.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Genreinput from "../components/Genreinput";
+import Genreinput from "../Components/Genreinput";
 
 export default class addGenre extends React.Component {
   //Write HTML inside render function
