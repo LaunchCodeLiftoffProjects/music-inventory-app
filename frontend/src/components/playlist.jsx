4f07c2ac49663121a45031975b3f91aa8f167038
lindskelsey <lindsaykelsey@gmail.com>
2019-12-13 18:40:58 -0600

    resolve merge conflicts

--- frontend/src/components/playlist.jsx
+++ frontend/src/components/playlist.jsx
@@ -19,22 +19,14 @@ export default class playlist extends React.Component {
             })
             .catch(error =>{
                 console.log(error)
-<<<<<<< HEAD
                 this.setState({errorMsg: 'Error retreiving data'})
-=======
-                this.setState({errorMsg: 'Error retreiving dara'})
->>>>>>> 97948fa9afbbdfeae7bc619ffeed0d555c934a47
             })
     }
 
 addTracks(id){
     this.setState({errorMsg: 'Function under construction'})
 }
-<<<<<<< HEAD
     
-=======
-
->>>>>>> 97948fa9afbbdfeae7bc619ffeed0d555c934a47
 
 
 
@@ -49,7 +41,6 @@ addTracks(id){
                 {
                     posts.length ?
                     posts.map(post => <div key={post.id}>
-<<<<<<< HEAD
                         {post.title} 
 
                         <a size="2" href={'/addtrackstoplaylist/' + post.id}>
@@ -59,16 +50,13 @@ Click to add tracks to this playlist
 </a>
                         
                         </div>):
-=======
-                        {post.title} {"                               "}{""}
-
-                        <Button variant="contained" size="small">Add</Button></div>):
->>>>>>> 97948fa9afbbdfeae7bc619ffeed0d555c934a47
                     null
                 }
                 { errorMsg ? <div>{errorMsg}</div> : null}
             
             </div>
+
+            
 
         )
     }
