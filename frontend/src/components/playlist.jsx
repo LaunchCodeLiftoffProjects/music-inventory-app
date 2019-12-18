a65a7dbb77853f6504a8a07bba766a235535c79f
Michael Behle <michaelbehle@yahoo.com>
2019-12-18 10:28:17 -0600

    small fixes

--- frontend/src/components/playlist.jsx
+++ frontend/src/components/playlist.jsx
@@ -1,63 +1,68 @@
-import React from 'react';
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 import Button from "@material-ui/core/Button";
 
-
 export default class playlist extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            posts: [],
-            errorMsg: ''
-        }
-    }
-    componentDidMount() {
-        axios.get('http://localhost:8080/playlist/viewplaylist')
-            .then(response => {
-                console.log(response)
-                this.setState({posts: response.data})
-            })
-            .catch(error =>{
-                console.log(error)
-                this.setState({errorMsg: 'Error retreiving data'})
-            })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      errorMsg: ""
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:8080/playlist/viewplaylist")
+      .then(response => {
+        console.log(response);
+        this.setState({ posts: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ errorMsg: "Error retreiving data" });
+      });
+  }
 
-addTracks(id){
-    this.setState({errorMsg: 'Function under construction'})
-}
-    
+  addTracks(id) {
+    this.setState({ errorMsg: "Function under construction" });
+  }
 
+  renderTableData() {
+    return this.state.posts.map((post, index) => {
+      const { id, title } = post;
+      return (
+        <tr key={id}>
+          <td>{title}</td>
+          <td>
+            <a size="2" href={"/viewtracksinaplaylist/" + post.id}>
+              View Tracks
+            </a>
+          </td>
+          <td>
+            <a size="2" href={"/addtrackstoplaylist/" + post.id}>
+              Add Tracks
+            </a>
+          </td>
+        </tr>
+      );
+    });
+  }
 
+  render() {
+    const { posts, errorMsg } = this.state;
+    return (
+      <div>
+        <h1 id="title">Playlists</h1>
 
-    render() {
-        const { posts, errorMsg } = this.state
-        return (
-            <div>
-                
-              <h1>  Playlist </h1>
-                         
-                              
-                {
-                    posts.length ?
-                    posts.map(post => <div key={post.id}>
-                        {post.title} 
-
-                        <a size="2" href={'/addtrackstoplaylist/' + post.id}>
-
-Click to add tracks to this playlist
-
-</a>
-                        
-                        </div>):
-                    null
-                }
-                { errorMsg ? <div>{errorMsg}</div> : null}
-            
-            </div>
-
-            
-
-        )
-    }
+        <table border="1" id="musictrack">
+          <tr>
+            <th>Playlist Title</th>
+            <th>View Tracks</th>
+            <th>Add Tracks</th>
+          </tr>
+          <tbody>{this.renderTableData()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
--- frontend/src/pages/addtrackstoplaylist.jsx
+++ frontend/src/pages/addtrackstoplaylist.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 import { withRouter } from "react-router";
 import { makeStyles } from "@material-ui/core/styles";
@@ -13,15 +14,74 @@ import {
 import Playlisttrackinput from "../components/Playlisttrackinput";
 
 export default class AddTrackPlaylist extends React.Component {
-  //Write HTML inside render function
-  constructor(props) {
-    super(props);
-    this.routeParam = props.match.params.id;
+  state = {
+    musictracks: []
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:8080/music-track").then(res => {
+      console.log(res);
+      this.setState({ musictracks: res.data });
+    });
+  }
+
+  handleClick = (event, id) => {
+    console.log(id);
+    const body = {
+      playlistid: this.props.match.params.id,
+      trackid: id
+    };
+    axios
+      .post("http://localhost:8080/playlist/addTrack", body)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  renderTableData() {
+    return this.state.musictracks.map((musictrack, index) => {
+      const { id, title, artist, type, genre } = musictrack;
+      return (
+        <tr key={id}>
+          <td>{title}</td>
+          {<td>{artist.name}</td>}
+          <td>{type}</td>
+          {<td>{genre.name}</td>}
+          <td>
+            {" "}
+            <button
+              value={id}
+              onClick={e => {
+                this.handleClick(e, id);
+              }}
+              type="submit"
+            >
+              Add
+            </button>
+          </td>
+        </tr>
+      );
+    });
   }
 
   render() {
-    const id = this.props.match.params.id;
-
-    return <div>{id}</div>;
+    return (
+      <div>
+        <h1 id="title">Add Tracks to the Playlist</h1>
+        <table border="1" id="musictrack">
+          <tr>
+            <th>Title of Song</th>
+            <th>Artist</th>
+            <th>Available On</th>
+            <th>Genre Name</th>
+          </tr>
+          <tbody>{this.renderTableData()}</tbody>
+        </table>
+      </div>
+    );
   }
 }
